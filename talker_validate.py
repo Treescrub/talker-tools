14042b6e5b226c393af3cfbf030a8923b3bbc6e4
Treescrub <treescrub@gmail.com>
2024-04-11 15:36:31 -0600

    Add skeleton script

---
+++ talker_validate.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def main():
+    tokens = lex()
+    
+    parse(tokens)
+
+
+def parse(tokens):
+    pass
+
+
+def lex():
+    pass
+    
+    
+if __name__ == "__main__":
+    main()
